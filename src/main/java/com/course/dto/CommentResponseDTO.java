3d18dc2ba264efcf4c58942f3810e60f61662e81
TriTai2004 <nguyentritai24042004@gmail.com>
2025-06-14 22:51:55 +0700

    [BE] add comment

---
+++ src/main/java/com/course/dto/CommentResponseDTO.java
@@ -0,0 +1,15 @@
+package com.course.dto;
+
+import java.util.Date;
+
+import lombok.Data;
+@Data
+public class CommentResponseDTO {
+    private Integer id;
+    private Integer rating;
+    private String comment;
+    private Integer parentComment;
+    private Date commentedDate;
+    private Integer accountId;
+    private Integer courseId;
+}
